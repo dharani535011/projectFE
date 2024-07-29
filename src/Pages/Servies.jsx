f251d4b50ca85cdccf85e3bd72c20036ee04726e
dharani535011 <dharani535011@gmail.com>
2024-07-29 09:58:10 +0530

    change

--- src/Pages/Servies.jsx
+++ src/Pages/Servies.jsx
@@ -1,111 +1,46 @@
-// import React, { useContext } from 'react'
-// import Whyas from '../Components/Whyas'
-// import Working from '../Components/Working'
-// import Gallery from '../Components/Gallery'
-// import Brands from '../Components/Brands'
-// import Foot from '../Components/Foot'
-// import Common from '../Components/Common'
-// import Nav2 from '../Components/Nav2'
-// import { useParams } from 'react-router-dom'
-// import { DataContext } from '../Contexts/DataContext'
-// import { OtherContext } from '../Contexts/OtherContext'
-// import Appointmentpopup1 from '../Components/Appointmentpopup1'
-// // import img from para.img
-// const Servies = () => {
-//   const {value}= useContext(OtherContext)
-//  const [popup,setpop]=value
-//   const {service}=useParams()
-//  const data= useContext(DataContext)
-//  const para=data.find((val)=>val.head==service)
- 
-//  const img = require(`${para.img}`).default
-//   return (
-//     <div>
-//       <Appointmentpopup1/>
-//     <div>
-//       <Nav2 name={service}/>
-//     </div>
-//     <div className='row row-cols-sm-1 row-cols-md-2 custom-des'>
-//       <div ><h1>{para.head}</h1>
-//       <h4>{para.para}</h4>
-//       <button onClick={()=>setpop(true)}>BOOK AN APPOINTMENT</button>
-//       </div>
-//       <div ><img src={img} alt="img" /></div>
-//     </div>
-//     <Whyas/>
-//     <Working/>
-//     <Gallery/>
-//     <Common/>
-//     <Brands/>
-//     <Foot/>
-//     </div>
-
-//   )
-// }
-
-// export default Servies
-
-
-import React, { useContext, useState, useEffect } from 'react';
-import Whyas from '../Components/Whyas';
-import Working from '../Components/Working';
-import Gallery from '../Components/Gallery';
-import Brands from '../Components/Brands';
-import Foot from '../Components/Foot';
-import Common from '../Components/Common';
-import Nav2 from '../Components/Nav2';
-import { useParams } from 'react-router-dom';
-import { DataContext } from '../Contexts/DataContext';
-import { OtherContext } from '../Contexts/OtherContext';
-import Appointmentpopup1 from '../Components/Appointmentpopup1';
-
+import React, { useContext } from 'react'
+import Whyas from '../Components/Whyas'
+import Working from '../Components/Working'
+import Gallery from '../Components/Gallery'
+import Brands from '../Components/Brands'
+import Foot from '../Components/Foot'
+import Common from '../Components/Common'
+import Nav2 from '../Components/Nav2'
+import { useParams } from 'react-router-dom'
+import { DataContext } from '../Contexts/DataContext'
+import { OtherContext } from '../Contexts/OtherContext'
+import Appointmentpopup1 from '../Components/Appointmentpopup1'
+import img from "../../public/Backgroundimg/p1.jpg"
 const Servies = () => {
-  const { value } = useContext(OtherContext);
-  const [popup, setpop] = value;
-  const { service } = useParams();
-  const data = useContext(DataContext);
-  const para = data.find((val) => val.head === service);
-  const [img, setImg] = useState(null);
-
-  useEffect(() => {
-    const loadImage = async () => {
-      try {
-        const image = await import(`${para.img}`);
-        setImg(image.default);
-      } catch (error) {
-        console.error("Error loading image:", error);
-      }
-    };
-
-    if (para) {
-      loadImage();
-    }
-  }, [para]);
-
+  const {value}= useContext(OtherContext)
+ const [popup,setpop]=value
+  const {service}=useParams()
+ const data= useContext(DataContext)
+ const para=data.find((val)=>val.head==service)
+ 
+  // console.log(para.img)
   return (
     <div>
-      <Appointmentpopup1 />
-      <div>
-        <Nav2 name={service} />
-      </div>
-      <div className='row row-cols-sm-1 row-cols-md-2 custom-des'>
-        <div>
-          <h1>{para.head}</h1>
-          <h4>{para.para}</h4>
-          <button onClick={() => setpop(true)}>BOOK AN APPOINTMENT</button>
-        </div>
-        <div>
-          {img && <img src={img} alt="img" />}
-        </div>
-      </div>
-      <Whyas />
-      <Working />
-      <Gallery />
-      <Common />
-      <Brands />
-      <Foot />
+      <Appointmentpopup1/>
+    <div>
+      <Nav2 name={service}/>
     </div>
-  );
-};
+    <div className='row row-cols-sm-1 row-cols-md-2 custom-des'>
+      <div ><h1>{para.head}</h1>
+      <h4>{para.para}</h4>
+      <button onClick={()=>setpop(true)}>BOOK AN APPOINTMENT</button>
+      </div>
+      <div ><img src={img} alt="img" /></div>
+    </div>
+    <Whyas/>
+    <Working/>
+    <Gallery/>
+    <Common/>
+    <Brands/>
+    <Foot/>
+    </div>
 
-export default Servies;
+  )
+}
+
+export default Servies
