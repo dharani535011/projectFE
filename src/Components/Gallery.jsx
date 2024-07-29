43989520065efcd6006b08b5f8f9b969f9aa51b6
dharani535011 <dharani535011@gmail.com>
2024-07-29 08:26:40 +0530

    update

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,6 +1,14 @@
 import React, { useContext } from 'react'
 import { OtherContext } from '../Contexts/OtherContext'
 import Appointmentpopup1 from './Appointmentpopup1'
+import img  from "../../public/Backgroundimg/gall1.jpg"   
+import img2 from "../../public/Backgroundimg/gall2.jpg" 
+import img3 from "../../public/Backgroundimg/gall3.jpg" 
+import img4 from "../../public/Backgroundimg/gall4.jpg"  
+import img5 from "../../public/Backgroundimg/gall5.jpg" 
+import img6 from "../../public/Backgroundimg/speedo.jpg" 
+
+
 
 const Gallery = () => {
   const {value}= useContext(OtherContext)
@@ -13,12 +21,12 @@ const Gallery = () => {
         Gallery
         </h1>
         <div className='box'>
-        <img src={`../../public/backgroundimg/gall1.jpg`} alt="img" />
-        <img src={`../../public/backgroundimg/gall2.jpg`} alt="img" />
-        <img src={`../../public/backgroundimg/gall3.jpg`} alt="img" />
-        <img src={`../../public/backgroundimg/gall4.jpg`} alt="img" />
-        <img src={`../../public/backgroundimg/gall5.jpg`} alt="img" />
-        <img src={`../../public/backgroundimg/enginebg.webp`} alt="img" />
+        <img src={img} alt="img" />
+        <img src={img2} alt="img" />
+        <img src={img3} alt="img" />
+        <img src={img4} alt="img" />
+        <img src={img5} alt="img" />
+        <img src={img6} alt="img" />
         </div>
        <button className='button' onClick={()=>setpop(true)}>BOOK AN APPOINTMENT</button>
         </div></>
